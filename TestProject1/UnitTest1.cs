a005d2faf4e51b9e3168ffbe20649beb9f388d47
NearKaneki <near55@yandex.ru>
2021-12-15 12:42:38 +0400

    Replace long locator

--- TestProject1/UnitTest1.cs
+++ TestProject1/UnitTest1.cs
@@ -27,7 +27,7 @@ namespace TestProject1
             driver.FindElement(By.XPath(".//div[@class='HeaderMenu__buttons  HeaderMenu__buttons_user']")).Click();
             driver.FindElement(By.Name("login")).SendKeys("7917300");
             driver.FindElement(By.Name("pass")).SendKeys("123456789");
-            Assert.IsFalse(driver.FindElements(By.XPath("//button[contains(@class, 'SignIn__button js--SignIn__action_sign-in  Button  jsButton Button_theme_primary Button_size_m Button_full-width') and not(@disabled)]")).Any(), "Кнопка не работает, так как введены не верные данные");
+            Assert.IsFalse(driver.FindElements(By.XPath("//button[contains(@class, 'SignIn__button') and not(@disabled)]")).Any());
         }
 
         [Test]
@@ -37,7 +37,7 @@ namespace TestProject1
             var element = driver.FindElement(By.CssSelector("a[data-search=\"хвалынск\"]"));
             new Actions(driver).MoveToElement(element).Perform();
             driver.FindElement(By.CssSelector("a[data-search=\"хвалынск\"]")).Click();
-            Assert.IsTrue(driver.FindElement(By.CssSelector("button[class=\"js--CitiesSearch-trigger MainHeader__open-text TextWithIcon\"]")).Text == "Хвалынск");
+            Assert.IsTrue(driver.FindElement(By.CssSelector("button[class=\"js--CitiesSearch-trigger MainHeader__open-text TextWithIcon\"]")).Text.Equals("Хвалынск"));
         }
 
         [Test]
@@ -56,8 +56,7 @@ namespace TestProject1
             el2.SendKeys(Keys.Enter);
             new WebDriverWait(driver, TimeSpan.FromSeconds(5))
                 .Until(x => driver.FindElement(By.XPath(".//*[text()='от 999 ₽ до 1 999 ₽']")));
-            var vals = driver.FindElements(By.CssSelector("div[class=\"product_data__gtm-js product_data__pageevents-js  ProductCardVertical js--ProductCardInListing " +
-                "ProductCardVertical_normal ProductCardVertical_shadow-hover ProductCardVertical_separated\"]"));
+            var vals = driver.FindElements(By.XPath(".//div[contains(@class,\"product_data__gtm-js\")]"));
             bool good = true;
             foreach(var i in vals)
             {
